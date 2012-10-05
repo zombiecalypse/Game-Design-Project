68a8755e14ea9efb13fe5b82bd1abd79558ecf51
Aaron Karper <akarper@students.unibe.ch>
2012-10-05 14:11:25 +0200

    Presentation & Demo upgrade

--- GestureSymbolsDemo/game.rb
+++ GestureSymbolsDemo/game.rb
@@ -14,20 +14,63 @@ class Array
   end
 
   def avg
-    sum/size
+    sum/size rescue 0
+  end
+end
+
+class Player
+  attr_accessor :symbols
+
+  attr_reader :x,:y, :last_dir
+
+  def x= nx
+    @last_dir = :left if nx < x
+    @last_dir = :right if nx > x
+    @x = nx
+  end
+
+  def y= ny
+    @last_dir = :up if ny < y
+    @last_dir = :down if ny > y
+    @y = ny
+  end
+
+
+  def initialize
+    @x, @y = 0,0
+
+    self.symbols = []
+  end
+
+  def symbol s
+    return nil unless s
+    puts s
+    symbols << s
+    rec = recognize(symbols)
+    self.symbols = [] if rec
+    return rec
+  end
+
+  def recognize(syms)
+    return :fire if syms == [:up, :up]
   end
 end
 
 class GameWindow < Gosu::Window
+  attr_accessor :player
   def initialize
     super 640, 480, false
     self.caption = "Gesture Symbols"
+    self.player = Player.new
     @@font ||= Gosu::Font.new(self, Gosu::default_font_name, 20)
-    @last_sym = nil
   end
 
   def update
     @gesture_buffer << [mouse_x,mouse_y] if @gesture
+    player.x -= 2 if button_down? Gosu::KbA
+    player.x += 2 if button_down? Gosu::KbD
+    player.y -= 2 if button_down? Gosu::KbW
+    player.y += 2 if button_down? Gosu::KbS
   end
 
   def draw_rect bl, tr, color
@@ -44,11 +87,48 @@ class GameWindow < Gosu::Window
     clear
     draw_buffer if @gesture_buffer
     draw_direction
-    draw_last_symbol
+    draw_symbols
+    draw_player
+    draw_spell_effect
   end
 
-  def draw_last_symbol 
-    @@font.draw(@last_symbol.to_s, 10, 10, 10, 1.0, 1.0, Gosu::Color::BLACK)
+  def draw_spell_effect
+    if @last_spell and (Gosu::milliseconds - @last_spell_time) < 1000
+      orange = Gosu::Color::rgba(0xdfa30d4a)
+      px, py = player.x + 320, player.y + 240
+      range = 100
+      if player.last_dir == :down
+        draw_triangle(
+          px, py, orange, 
+          px + range, py + range, orange, 
+          px - range, py + range, orange)
+      elsif player.last_dir == :up or player.last_dir.nil?
+        draw_triangle(
+          px, py, orange, 
+          px + range, py - range, orange, 
+          px - range, py - range, orange)
+      elsif player.last_dir == :right
+        draw_triangle(
+          px, py, orange, 
+          px + range, py - range, orange, 
+          px + range, py + range, orange)
+      elsif player.last_dir == :left
+        draw_triangle(
+          px, py, orange, 
+          px - range, py - range, orange, 
+          px - range, py + range, orange)
+      end
+    end
+  end
+
+  def draw_player
+    x,y = player.x+320, player.y+240
+    draw_rect([x - 10, y-10], [x+10, y+10], Gosu::Color::RED)
+  end
+
+  def draw_symbols 
+    @@font.draw(player.symbols.collect(&:to_s).join(", "), 10, 10, 10, 1.0, 1.0, Gosu::Color::BLACK)
+    @@font.draw(@last_spell, 300, 400, 10, 1.0, 1.0, Gosu::Color::BLACK)
   end
 
   def button_down id
@@ -64,9 +144,17 @@ class GameWindow < Gosu::Window
   end
 
   @@button_mapping = {
-    Gosu::MsRight => :mouse_right
+    Gosu::MsRight => :mouse_right,
+    Gosu::MsLeft  => :mouse_left
   }  
   def needs_cursor?; true; end
+
+  def mouse_left_down
+    @player.symbols = []
+  end
+
+  def mouse_left_up
+  end
 
   def mouse_right_down
     @gesture = true
@@ -89,16 +177,16 @@ class GameWindow < Gosu::Window
   def mouse_right_up
     @gesture = false
     symbol = recognize(@gesture_buffer)
-    puts symbol
-    @last_symbol = symbol
+    @last_spell  = player.symbol(symbol)
+    @last_spell_time = Gosu::milliseconds
   end
 
   def recognize list
     xs,ys = list.transpose
     dx,dy = [xs,ys].collect {|c| c.differences.avg}
-    phi = Math.atan2(dx,dy)
+    phi = Math.atan2(dx,dy) rescue 0
     puts "dX: #{dx} dY: #{dy} phi: #{phi}"
-    @last_dir_x, @last_dir_y = [dx,dy].collect {|e| e/Math.hypot(dx,dy)}
+    @last_dir_x, @last_dir_y = [dx,dy].collect {|e| (e/Math.hypot(dx,dy) rescue 0)}
     return :down  if dy > 0 and dy.abs > dx.abs
     return :left  if dx < 0 and dy.abs < dx.abs
     return :up    if dy < 0 and dy.abs > dx.abs
