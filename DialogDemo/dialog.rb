ad5defe3b2a6e3180201585ee1949118ab35a6a9
Clood-Desktop <c.descombes@students.unibe.ch>
2012-10-12 09:48:34 +0200

    Add not finished demo for dialogs

---
+++ DialogDemo/dialog.rb
@@ -0,0 +1,24 @@
+# /usr/bin env Ruby
+require 'rubygems'
+require 'chingu'
+
+include Gosu
+
+class Dialog
+	attr_accessor :question, :answer1, :answer2, :answer3, :next_dialog
+	attr_reader :answer_count
+	
+	def initialize (question, answer1, answer2, answer3)
+		@question = question
+		@answer1 = answer1
+		@answer2 = answer2
+		@answer3 = answer3
+		@answer_count = 3
+	end
+	
+	def ask
+		dialog_pack = [question, answer1, answer2,answer3]
+		dialog pack
+	end
+	
+end
