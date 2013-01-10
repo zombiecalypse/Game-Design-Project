f4e9fe62384cb2f12865e37aff1e5a50e1d33c69
Clood-Laptop <c.descombes@students.unibe.ch>
2013-01-10 09:51:32 +0100

    BFS for pathfining added

--- GamePrototype/object_traits/mover.rb
+++ GamePrototype/object_traits/mover.rb
@@ -57,7 +57,7 @@ module Chingu::Traits
         @y += dy
       else
         if P[@goal.x,@goal.y] != @old_goal_position
-          @path = recalculate_path_to @goal
+          @path = recalculate_path_to @goal if (@path.nil? || @path.empty?)
           @old_goal_position = P[@goal.x, @goal.y]
         end
 
@@ -67,7 +67,7 @@ module Chingu::Traits
 
     # fill AI here, seriously... do!
     def recalculate_path_to g
-      @path = [g]
+      @path = BFS.new(Pos.new(x,y),g,parent).path;
     end
 
     def move_along_path
@@ -90,7 +90,128 @@ module Chingu::Traits
       super
       move
     end
-
+    
+    class BFS
+    	attr_accessor :path
+    	def initialize start, goal, level                           
+    		@start = Pos.new(((start.x-16) / 32).round * 32 + 16, ((start.y-16) / 32).round * 32 + 16)
+    		@goal = goal
+    		@goal_pos = Pos.new(((goal.x-16) / 32).round * 32 + 16, ((goal.y-16) / 32).round * 32 + 16)
+    		@level = level
+    		t = Node.new(nil, @start)
+    		@tiles = [t]
+    		@queue = [t]	
+    		goal_node = search
+    		@path = get_path goal_node
+    	end
+    	
+    	def search
+    		current_node = nil
+    		new_pos = nil
+    		new_node = nil
+    		while not @queue.empty? do
+    			count = 0
+    			current_node = @queue.shift
+    			new_pos = Pos.new(current_node.pos.x + 32, current_node.pos.y)
+    			if ((not pos_blocked? new_pos) && (not exists? new_pos))
+    				new_node = Node.new(current_node, new_pos)
+    				if (new_pos.equal? @goal_pos)
+    					return new_node
+    				end
+    				@tiles << new_node
+    				@queue << new_node
+    			end
+    			new_pos = Pos.new(current_node.pos.x - 32, current_node.pos.y)
+    			if ((not pos_blocked? new_pos) && (not exists? new_pos))
+    				new_node = Node.new(current_node, new_pos)
+    				if (new_pos.equal? @goal_pos)
+    					return new_node
+    				end
+    				@tiles << new_node
+    				@queue << new_node
+    			end
+    			new_pos = Pos.new(current_node.pos.x, current_node.pos.y + 32)
+    			if ((not pos_blocked? new_pos) && (not exists? new_pos))
+    				new_node = Node.new(current_node, new_pos)
+    				if (new_pos.equal? @goal_pos)
+    					return new_node
+    				end
+    				@tiles << new_node
+    				@queue << new_node
+    			end
+    			new_pos = Pos.new(current_node.pos.x, current_node.pos.y - 32)
+    			if ((not pos_blocked? new_pos) && (not exists? new_pos))
+    				new_node = Node.new(current_node, new_pos)
+    				if (new_pos.equal? @goal_pos)
+    					return new_node
+    				end
+    				@tiles << new_node
+    				@queue << new_node
+    			end
+    		end
+    	end
+    	
+    	def get_path node
+    		current_node = node
+    		path = [current_node.pos]
+    		while(current_node.parent?)
+    			current_node = current_node.parent
+    			path << current_node.pos
+    		end
+    		path.reverse
+    	end
+    	
+    	def exists? pos
+    		@tiles.each { |n| return true if pos.equal? n.pos}
+    		false
+    	end
+    	
+    	def pos_blocked? pos
+    		for i in pos.x-16..pos.x+16
+    			for j in pos.y-16..pos.y+16
+    				return true if @level.blocked?(i, j)
+    			end
+    		end
+    		false
+    	end
+    	
+    end
+    
+    class Node
+    	attr_accessor :pos, :parent, :children
+    	
+    	def initialize parent, pos
+    		@pos = pos
+    		@parent = parent
+    		@children = []
+    	end
+    	
+    	def parent?
+    		return (not @parent.nil?)
+    	end
+    	
+    	def to_s
+    		return pos.to_s
+    	end
+    end
+    
+    class Pos
+    	attr_accessor :x, :y
+    	
+    	def initialize x, y
+    		@x = x
+    		@y = y
+    	end
+    	
+    	def equal? other
+    		return x == other.x && y == other.y
+    	end
+    	
+    	def to_s
+    		return "(#{x},#{y})"
+    	end
+    end
+    
     private
     def log_debug(&b)
       self.class.log_debug(&b)
