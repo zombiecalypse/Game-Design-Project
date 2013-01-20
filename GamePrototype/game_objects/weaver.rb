d2d82ce987c7d8a24002ecf7a7212d8db56cfa26
Aaron Karper <akarper@students.unibe.ch>
2013-01-20 17:33:26 +0100

    Choreography

--- GamePrototype/game_objects/weaver.rb
+++ GamePrototype/game_objects/weaver.rb
@@ -24,6 +24,12 @@ module Objects
         super(opts.merge image: @current_animation.next)
       end
 
+      def cooldown_time
+        return 1000 if state == :all_out
+
+        2000
+      end
+
       # An attack, that takes some time to lift the leg, but does devastating
       # damage.
       def pierce p
@@ -31,7 +37,7 @@ module Objects
         after(1000) do
           log_debug {"pierce at #{[p.x,p.y]}"}
         end.then do 
-          after(1000) { @cooldown = nil }
+          after(cooldown_time) { @cooldown = nil }
         end
       end
 
@@ -39,16 +45,16 @@ module Objects
       def slash p
         @cooldown = true
         log_debug {"slash at #{[p.x,p.y]}"}
-        after(1000) do
+        after(cooldown_time) do
           @cooldown = nil
         end
       end
 
       # An attack, that shoots an ball of immobilizing slime at the player
       def spit p
-        log_debug {"spit at #{[p.x,p.y]}"}
         @cooldown = true
-        after(1000) do
+        log_debug {"spit at #{[p.x,p.y]}"}
+        after(cooldown_time) do
           @cooldown = nil
         end
       end
@@ -57,16 +63,32 @@ module Objects
       def stomp p
         log_debug {"stomp at #{[p.x,p.y]}"}
         @cooldown = true
-        after(1000) do
+        after(cooldown_time) do
           @cooldown = nil
         end
       end
 
       def piercing_distance; 200; end
 
-      while_in(:playing_with_hero) do
+      def on_harm x
+        if hp < 100 and state == :second_playing_with_hero
+          log_debug {"Go hiding second time"}
+          self.state = :hiding 
+          after(20000) {self.state = :all_out}
+        elsif hp < 250 and state == :playing_with_hero
+          log_debug {"Go hiding first time"}
+          self.state = :hiding 
+          after(20000) {self.state = :second_playing_with_hero}
+        end
+      end
+
+      def harm x
+        super x unless state == :hiding
+      end
+
+      def playing_with_hero
         keep_distance the(Player), piercing_distance
-        next if @cooldown
+        return if @cooldown
         d_to_player = d(self, the(Player))
         if d_to_player > piercing_distance*1.5
           spit the Player
@@ -77,16 +99,38 @@ module Objects
         end
       end
 
+      def summon_spiders
+        log_debug {"Summoning new spiders"}
+      end
+
+      while_in(:playing_with_hero) { playing_with_hero }
+
+      while_in(:second_playing_with_hero) { playing_with_hero }
+
       while_in :hiding do
         # move to save spot
         # become invincible
         # summon some enemies 
+        move_away_from the Player
+        every(3000) do
+          summon_spiders
+        end
       end
 
       while_in :all_out do
         # become fast
-        # slash or stomp
+        @speed = 5
         # move toward player
+        d_to_player = d(self, the(Player))
+        keep_distance the(Player), piercing_distance*0.75
+        next unless d_to_player < piercing_distance*1.5
+
+        # slash or stomp
+        if rand < 0.25
+          stomp the Player
+        else
+          slash the Player
+        end
       end
     end
   end
