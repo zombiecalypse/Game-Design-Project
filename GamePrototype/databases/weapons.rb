7102699ad45415bb53e62131ae28ed3cb1ff3dec
Aaron Karper <akarper@students.unibe.ch>
2013-01-27 16:01:44 +0100

    Added Weapons database
    with default

---
+++ GamePrototype/databases/weapons.rb
@@ -0,0 +1,16 @@
+require_relative 'weapons/pistol'
+require_relative 'weapons/sword'
+
+module Weapons
+  def self.all; @all; end
+  def self.weapon name, x, opts={}
+    @all ||= {}
+    @all[name] = x
+    @default = x if opts[:default]
+  end
+
+  def self.default; @default; end
+
+  weapon :sword, Sword
+  weapon :pistol, Pistol, default: true
+end
--- GamePrototype/menu/player_daemon.rb
+++ GamePrototype/menu/player_daemon.rb
@@ -1,8 +1,7 @@
 require 'singleton'
 require_relative '../helpers/the'
 require_relative '../interface/hud_interface'
-require_relative '../databases/weapons/pistol'
-require_relative '../databases/weapons/sword'
+require_relative '../databases/weapons'
 # Does the book-keeping for the player. Has the persistent instances for the
 # player's connections and can then be connected to different Player instances
 # across levels.
@@ -79,7 +78,7 @@ class PlayerDaemon
 
     x,y = state[point] if point and state.respond_to?(:[])
 
-    @player = Objects::Player.create(info.merge! x: x, y: y, level: state, weapon: Sword.new)
+    @player = Objects::Player.create(info.merge! x: x, y: y, level: state, weapon: Weapons::default.new)
     @hud.player = @player
     @player.input = { 
       holding_a:             :move_left, 
